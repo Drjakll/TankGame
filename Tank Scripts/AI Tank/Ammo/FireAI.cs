60337e9d52ff9e3abea158d8263c2fa3ea529c8d
Justin Zhu <dr_kimsora@yahoo.com>
2018-07-14 18:22:23 -0700

    Tank Scripts

---
+++ Tank Scripts/AI Tank/Ammo/FireAI.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+/// <summary>
+/// Use to fire ammo within the direction of the barrel. Should create an empty GameObject infront of the tank barrel and attach it to the empty GameObject.
+/// </summary>
+public class FireAI : MonoBehaviour {
+    private Vector3 Force;
+    private GimTrim Anker; //Needed to help determine the angle of the barrel. It's located between the turrent body and the barrel.
+    private GameObject ToBeFire;
+    public GameObject Ammo;
+	// Use this for initialization
+	void Start () {
+        Anker = GetComponentInParent<TurrentMoveAI>().GetComponentInChildren<GimTrim>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void Fire(float speed)
+    {
+        ToBeFire = (GameObject)Instantiate(Ammo);
+        ToBeFire.transform.position = this.gameObject.transform.position;
+        ToBeFire.transform.eulerAngles = transform.eulerAngles;
+        float magnitudeForce = speed * ToBeFire.GetComponent<Rigidbody>().mass / .09f;
+        ConstantForce CF = ToBeFire.GetComponent<ConstantForce>();
+        Vector3 AimDirection = (transform.position - Anker.transform.position).normalized;
+        CF.force = magnitudeForce * AimDirection;
+        StartCoroutine(applyForce(ToBeFire));
+    }
+
+    protected IEnumerator applyForce(GameObject toBeFire)
+    {
+        yield return new WaitForSeconds(.07f);
+        toBeFire.GetComponent<ConstantForce>().force = Vector3.zero;
+    }
+}
---
+++ Tank Scripts/Player Tank/TrackMovement.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrackMovement : WheelMovement {
+
+	// Use this for initialization
+	void Start () {
+        base.Init();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButtonDown(0))
+        {
+            leftMouseDown = true;
+        }
+        else if (Input.GetMouseButtonUp(0))
+        {
+            leftMouseDown = false;
+        }
+        if (Input.GetAxis("Horizontal") != 0 && !leftMouseDown)
+        {
+            trackAngle = trackAngle + Input.GetAxis("Horizontal") * rotationSpeed * Time.deltaTime;
+            trackAngle = Mathf.Clamp(trackAngle, minTrackAngle, maxTrackAngle);
+            transform.localEulerAngles = new Vector3(0, trackAngle, 0);
+        }
+        else
+        {
+            if (transform.localEulerAngles.y <= 10)
+            {
+                transform.Rotate(0, -Time.deltaTime * transform.localEulerAngles.y, 0);
+                trackAngle = trackAngle - Time.deltaTime * transform.localEulerAngles.y;
+            }
+            else
+            {
+                transform.Rotate(0, -Time.deltaTime * (transform.localEulerAngles.y - 360.0f), 0);
+                trackAngle = trackAngle - Time.deltaTime * (transform.localEulerAngles.y - 360.0f);
+            }
+        }
+    }
+}
