e0013ebb68e9ff152e589f1109d2c135e147319c
Justin Zhu <dr_kimsora@yahoo.com>
2018-07-14 22:11:28 -0700

    Update Identifier.cs

--- Scenes Scripts/Identifier.cs
+++ Scenes Scripts/Identifier.cs
@@ -1,7 +1,9 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+///<Summary>
+///A parent class template that will be inherit by other types of identifier classes.
+///</Summary>
 abstract public class Identifier : MonoBehaviour {
 
 	// Use this for initialization
